29b3af6d710a9bfa611bb3ac335e8fd641a2ce18
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-05-02 12:06:55 -0500

    database 2

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,22 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const uri = process.env.DB_URI
+
+mongoose.connect(
+  uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+)
+
+mongoose.connection.on('error', console.error.bind(console, 'Error al conectarse con la base de datos.'));
+
+mongoose.connection.once('open', () => {
+  console.log('Conexión con la base de datos iniciada.');
+});
+
+export default mongoose
+
