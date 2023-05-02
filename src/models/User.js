e748848caf7c42b0e3eafbe34ab8a93b84011901
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-05-02 10:26:49 -0500

    UserSchema 2

--- src/models/User.js
+++ src/models/User.js
@@ -20,6 +20,23 @@ const userSchema = new Schema({
   }
 })
 
+userSchema.virtual('password')
+  .set(function (password) {
+    this._password = password;
+    this.passwordHash = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
+userSchema.methods.encryptPassword = function (password) {
+  return bcrypt.hashSync(password, 10);
+};
 
+userSchema.methods.isValidPassword = function (password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
 
+const User = model('User', userSchema);
+
+export default User;
